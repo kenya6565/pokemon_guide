6652565aefc506b85bcc42dd5556d5e7b6a5583d
kenya6565 <ken.australia0605@gmail.com>
2023-05-09 01:10:05 +0900

    create getPokemons.graphql

---
+++ frontend/pages/pokemons.tsx
@@ -0,0 +1,36 @@
+import { GetPokemons } from '../src/generated/graphql';
+import { useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const GET_POKEMONS = gql`
+  query GetPokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      id
+      name
+    }
+  }
+`;
+
+const PokemonsPage = () => {
+  const { loading, error, data } = useQuery<GetPokemons>(GET_POKEMONS, {
+    variables: { limit: 20, offset: 0 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Pokémon List</h1>
+      <ul>
+        {data?.pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            {pokemon.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PokemonsPage;
--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -1,9 +1,11 @@
 import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -37,3 +39,50 @@ export type QueryPokemonsArgs = {
   limit?: InputMaybe<Scalars['Int']>;
   offset?: InputMaybe<Scalars['Int']>;
 };
+
+export type GetPokemonsQueryVariables = Exact<{
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+}>;
+
+
+export type GetPokemonsQuery = { __typename?: 'Query', pokemons: Array<{ __typename?: 'Pokemon', id: string, name: string }> };
+
+
+export const GetPokemonsDocument = gql`
+    query GetPokemons($limit: Int, $offset: Int) {
+  pokemons(limit: $limit, offset: $offset) {
+    id
+    name
+  }
+}
+    `;
+
+/**
+ * __useGetPokemonsQuery__
+ *
+ * To run a query within a React component, call `useGetPokemonsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetPokemonsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetPokemonsQuery({
+ *   variables: {
+ *      limit: // value for 'limit'
+ *      offset: // value for 'offset'
+ *   },
+ * });
+ */
+export function useGetPokemonsQuery(baseOptions?: Apollo.QueryHookOptions<GetPokemonsQuery, GetPokemonsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetPokemonsQuery, GetPokemonsQueryVariables>(GetPokemonsDocument, options);
+      }
+export function useGetPokemonsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetPokemonsQuery, GetPokemonsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetPokemonsQuery, GetPokemonsQueryVariables>(GetPokemonsDocument, options);
+        }
+export type GetPokemonsQueryHookResult = ReturnType<typeof useGetPokemonsQuery>;
+export type GetPokemonsLazyQueryHookResult = ReturnType<typeof useGetPokemonsLazyQuery>;
+export type GetPokemonsQueryResult = Apollo.QueryResult<GetPokemonsQuery, GetPokemonsQueryVariables>;
