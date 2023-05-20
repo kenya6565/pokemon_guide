e6a5475a9524d49d8aef1931e0bb03c201bbe7db
kenya6565 <ken.australia0605@gmail.com>
2023-05-20 21:37:06 +0900

    deal with dark mode of Chrome

--- frontend/pages/pokemons.tsx
+++ frontend/pages/pokemons.tsx
@@ -34,12 +34,12 @@ const PokemonsPage = () => {
 
   return (
     <div style={{width: "100%"}}>
-    <h1 style={{textAlign: "center", fontSize: "3rem", fontWeight: "bold", marginBottom: "2rem"}}>Pokémon List</h1>
+    <h1 style={{textAlign: "center", fontSize: "3rem", fontWeight: "bold", marginBottom: "2rem", color: "black"}}>Pokémon List</h1>
     {pokemonRows.map((pokemonRow, rowIndex) => (
       <div key={rowIndex} style={{display: "flex", justifyContent: "space-around", marginBottom: "1rem"}}>
         {pokemonRow.map((pokemon) => (
           <div key={pokemon.id} style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "1rem", background: "#fff", borderRadius: "12px", boxShadow: "0 5px 15px rgba(0,0,0,0.1)", transition: "transform 0.3s ease-in-out"}}>
-            <h2 style={{fontSize: "1.2rem", fontWeight: "600", marginBottom: "1rem"}}>{pokemon.name}</h2>
+            <h2 style={{fontSize: "1.2rem", fontWeight: "600", marginBottom: "1rem", color: "black"}}>{pokemon.name}</h2>
             <div style={{width: "100px", height: "100px", position: "relative"}}>
               <Image src={pokemon.image} alt={pokemon.name} layout="fill" objectFit="contain"/>
             </div>
